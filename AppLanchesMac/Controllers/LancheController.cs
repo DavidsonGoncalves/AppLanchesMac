7fbd824ebae5a6f3c353115205e94ef7678ab6b9
Davidson <davidson.cg7@gmail.com>
2024-08-16 20:03:57 -0300

    Creating Lanches view and using a little of bootstrap.

---
+++ AppLanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,22 @@
+﻿using AppLanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppLanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+
+        private readonly ILancheRepository _lancherepository;
+
+        public LancheController(ILancheRepository lancherepository)
+        {
+            _lancherepository = lancherepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanche = _lancherepository.Lanches;
+            return View(lanche);
+        }
+    }
+}
