61b4580bd9d7016cfc00c0ad19ab169e98ccd4f7
Davidson <davidson.cg7@gmail.com>
2024-08-28 20:48:10 -0300

    crating components and taghelpers

---
+++ AppLanchesMac/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,31 @@
+﻿using AppLanchesMac.Models;
+using AppLanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppLanchesMac.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+           var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+            
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVM);
+        }
+    }
+}
