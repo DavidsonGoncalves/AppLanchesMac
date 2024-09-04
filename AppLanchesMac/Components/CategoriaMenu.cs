a702441b8640c25b8937a3ce82ce54bbae5c7852
Davidson <davidson.cg7@gmail.com>
2024-09-03 21:48:44 -0300

    Adding a new view component to show the lanche categoria

---
+++ AppLanchesMac/Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using AppLanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppLanchesMac.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke ()
+        {
+            var categorias = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categorias);
+        }
+    }
+}
