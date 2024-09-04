8a5485e1493fd92b7914d5f9a410ee766d28baa2
Davidson <davidson.cg7@gmail.com>
2024-09-03 21:23:24 -0300

    creeating endpoints to acces pages

--- AppLanchesMac/Controllers/LancheController.cs
+++ AppLanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
-﻿using AppLanchesMac.Repositories;
+﻿using AppLanchesMac.Models;
+using AppLanchesMac.Repositories;
 using AppLanchesMac.Repositories.Interfaces;
 using AppLanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -15,15 +16,33 @@ namespace AppLanchesMac.Controllers
             _lancherepository = lancherepository;
         }
 
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
             //var lanche = _lancherepository.Lanches;
             //return View(lanche);
+            //var lancheListViewModel = new LanchesListViewModel();
+            //lancheListViewModel.Lanches = _lancherepository.Lanches;
+            //lancheListViewModel.CategoriaAtual = "Categoria";
 
-            var lancheListViewModel = new LanchesListViewModel();
-            lancheListViewModel.Lanches = _lancherepository.Lanches;
-            lancheListViewModel.CategoriaAtual = "Categoria";
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
 
+            if(string.IsNullOrEmpty(categoria) || !_lancherepository.Lanches.Any(l => l.Categoria.CategoriaNome.Equals(categoria, StringComparison.OrdinalIgnoreCase)))
+            {
+                lanches = _lancherepository.Lanches.OrderBy(l => l.LancheId);
+                categoriaAtual = "Todos os lanches";
+            }
+            else
+            {
+                lanches = _lancherepository.Lanches.Where(l => l.Categoria.CategoriaNome.Equals(categoria, StringComparison.OrdinalIgnoreCase)).OrderBy(l => l.Nome);
+            }
+
+
+            var lancheListViewModel = new LanchesListViewModel()
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
+            };
             return View(lancheListViewModel);
 
         }
