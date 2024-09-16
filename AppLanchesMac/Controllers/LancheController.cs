bd725644b7c1458308d51d5863575bf800543d95
Davidson <davidson.cg7@gmail.com>
2024-09-16 18:39:15 -0300

    adding new layouts

--- AppLanchesMac/Controllers/LancheController.cs
+++ AppLanchesMac/Controllers/LancheController.cs
@@ -3,6 +3,7 @@ using AppLanchesMac.Repositories;
 using AppLanchesMac.Repositories.Interfaces;
 using AppLanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
 
 namespace AppLanchesMac.Controllers
 {
@@ -51,6 +52,34 @@ namespace AppLanchesMac.Controllers
         {
             var lanche= _lancherepository.Lanches.FirstOrDefault(l =>  l.LancheId == lancheId);
             return View(lanche);
+        }
+
+        public ViewResult Search(string searchString)
+        {
+
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
+
+            if (string.IsNullOrEmpty(searchString))
+            {
+                lanches = _lancherepository.Lanches.OrderBy(p => p.LancheId);
+                categoriaAtual = "Todos os Lanches";
+            }
+            else
+            {
+                lanches = _lancherepository.Lanches.Where(p=> p.Nome.ToLower().Contains(searchString.ToLower()));
+                if(lanches.Any())
+                {
+                    categoriaAtual = "Lanches";
+                }
+                else
+                {
+                    categoriaAtual= "Nenhum Lanche foi encontrado";
+                }
+
+            }
+            return View("~/Views/Lanche/List.cshtml", new LanchesListViewModel { Lanches = lanches, CategoriaAtual = categoriaAtual });
+
         }
     }
 }
---
+++ AppLanchesMac/Controllers/PedidoController.cs
@@ -0,0 +1,63 @@
+﻿using AppLanchesMac.Models;
+using AppLanchesMac.Repositories.Interfaces;
+//using AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppLanchesMac.Controllers
+{
+    public class PedidoController : Controller
+    {
+
+        private readonly IpedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhocompra;
+
+        public PedidoController(IpedidoRepository pedidoRepository, CarrinhoCompra carrinhocompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhocompra = carrinhocompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = decimal.Zero;
+
+            List<CarrinhoCompraItem> items = _carrinhocompra.GetCarrinhoCompraItens();
+            _carrinhocompra.CarrinhoCompraItems = items;
+
+            if (_carrinhocompra.CarrinhoCompraItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio");
+            }
+
+            foreach (var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            if (ModelState.IsValid)
+            {
+                _pedidoRepository.CriarPedido(pedido);
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido";
+                ViewBag.TotalPedido = _carrinhocompra.GetCarrinhoCompraTotal();
+
+                _carrinhocompra.LimparCarrinho();
+
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
+        }
+
+    }
+}
